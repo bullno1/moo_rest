85156dddc49b3ccc4039638a6416b773faf12f73
bullno1 <thebullno1@gmail.com>
2015-01-07 04:46:03 +0800

    Better error handling and a separate parse_body phase

--- src/moo_rest.erl
+++ src/moo_rest.erl
@@ -12,15 +12,6 @@
 	media_type
 }).
 
-render_error(unauthorized, _, Req, State) ->
-	{<<>>, Req, State};
-render_error(Error, {<<"text">>, <<"html">>, _}, Req, State) ->
-	{io_lib:format("~p", [Error]), Req, State};
-render_error(Error, {<<"application">>, <<"json">>, _}, Req, State) ->
-	{io_lib:format("\"~p\"", [Error]), Req, State};
-render_error(_, undefined, Req, State) ->
-	{<<>>, Req, State}.
-
 upgrade(Req, Env, Module, Opts) ->
 	State = #state{
 		env = Env,
@@ -29,6 +20,13 @@ upgrade(Req, Env, Module, Opts) ->
 		handler_opts = Opts
 	},
 	method_known(Req, State).
+
+render_error(Error, {<<"text">>, <<"html">>, _}, Req, State) ->
+	{io_lib:format("~p", [Error]), Req, State};
+render_error(Error, {<<"application">>, <<"json">>, _}, Req, State) ->
+	{io_lib:format("\"~p\"", [Error]), Req, State};
+render_error(_, undefined, Req, State) ->
+	{<<>>, Req, State}.
 
 method_known(Req, State) ->
 	{Method, Req2} = cowboy_req:method(Req),
@@ -40,7 +38,7 @@ method_known(Req, State) ->
 	end.
 
 method_allowed(Req, #state{method=Method, handler=Module} = State) ->
-	case erlang:function_exported(Module, Method, method_arity(Method)) of
+	case erlang:function_exported(Module, Method, 2) of
 		true ->
 			negotiate_content_type(Req, State);
 		false ->
@@ -128,6 +126,9 @@ choose_if_params_match(Solution, ParamsA, ParamsB) ->
 		false -> no_solution
 	end.
 
+build_content_type({Type, Subtype, Params}) ->
+	[Type, $/, Subtype | [[<<"; ">>, Attr, $=, Value] || {Attr, Value} <- Params]].
+
 variances(DefaultVariances, Req, #state{handler=Handler} = State) ->
 	try call(Handler, variances, [], DefaultVariances) of
 		[] ->
@@ -151,13 +152,12 @@ init(Req, #state{handler=Module, handler_opts=Opts} = State) ->
 			respond(400, Req2, State#state{handler_state=HandlerState, error=bad_request});
 		{{error, Reason}, Req2, HandlerState} ->
 			respond(400, Req2, State#state{handler_state=HandlerState, error=Reason});
-		{halt, Req2} ->
-			stop(Req2, State)
+		{halt, Req2, HandlerState} ->
+			terminate(Req2, State#state{handler_state=HandlerState})
 	catch
 		Class:Error ->
 			error_terminate({Class, Error}, rest_init, Req, State)
 	end.
-
 
 authorize(Req, #state{method = Method,
 					  handler = Handler,
@@ -170,14 +170,15 @@ authorize(Req, #state{method = Method,
 		{{unauthorized, AuthHeader}, Req2, NewHandlerState} ->
 			Req3 = cowboy_req:set_resp_header(<<"www-authenticate">>, AuthHeader, Req2),
 			respond(401, Req3, State#state{handler_state=NewHandlerState, error=unauthorized});
-		{halt, Req2} ->
-			stop(Req2, State)
+		{halt, Req2, NewHandlerState} ->
+			terminate(Req2, State#state{handler_state=NewHandlerState})
 	catch
 		Class:Error ->
 			error_terminate({Class, Error}, authorize, Req, State)
 	end.
 
-process_request(Method, Req, #state{handler=Handler, handler_state=HandlerState} = State) when Method =:= get; Method =:= head ->
+process_request(Method, Req, #state{handler=Handler, handler_state=HandlerState} = State) when
+	  Method =:= get; Method =:= head ->
 	try Handler:Method(Req, HandlerState) of
 		{ok, Req2, NewHandlerState} ->
 			render(render_resource, Req2, State#state{handler_state=NewHandlerState});
@@ -191,8 +192,8 @@ process_request(Method, Req, #state{handler=Handler, handler_state=HandlerState}
 		{{moved_temporarily, NewUrl}, Req2, NewHandlerState} ->
 			Req3 = cowboy_req:set_resp_header(<<"location">>, NewUrl, Req2),
 			respond(307, Req3, State#state{handler_state=NewHandlerState});
-		{halt, Req2} ->
-			stop(Req2, State)
+		{halt, Req2, NewHandlerState} ->
+			terminate(Req2, State#state{handler_state=NewHandlerState})
 	catch
 		Class:Error ->
 			error_terminate({Class, Error}, Method, Req, State)
@@ -202,7 +203,7 @@ process_request(Method, Req, State) when
 	  Method =:= put; Method =:= post; Method =:= patch ->
 	case cowboy_req:parse_header(<<"content-type">>, Req) of
 		{ok, ContentType, Req2} ->
-			accept_resource(ContentType, Method, Req2, State);
+			parse_body(ContentType, Method, Req2, State);
 		{error, badarg} ->
 			respond(415, Req, State#state{error=unsupported})
 	end;
@@ -220,14 +221,33 @@ process_request(delete, Req, #state{handler=Handler, handler_state=HandlerState}
 			respond(301, Req3, State#state{handler_state=NewHandlerState});
 		{{moved_temporarily, NewUrl}, Req2, NewHandlerState} ->
 			Req3 = cowboy_req:set_resp_header(<<"location">>, NewUrl, Req2),
-			respond(307, Req3, State#state{handler_state=NewHandlerState})
+			respond(307, Req3, State#state{handler_state=NewHandlerState});
+		{halt, Req2, NewHandlerState} ->
+			terminate(Req2, State#state{handler_state=NewHandlerState})
 	catch
 		Class:Error ->
 			error_terminate({Class, Error}, delete, Req, State)
 	end.
 
-accept_resource(ContentType, Method, Req, #state{handler=Handler, handler_state=HandlerState} = State) ->
-	try Handler:Method(ContentType, Req, HandlerState) of
+parse_body(ContentType, Method, Req,  #state{handler=Handler, handler_state=HandlerState} = State) ->
+	try call(Handler, parse_body, [ContentType, Req, HandlerState], {ok, Req, HandlerState}) of
+		{ok, Req2, NewHandlerState} ->
+			accept_resource(Method, Req2, State#state{handler_state=NewHandlerState});
+		{error, Req2, NewHandlerState} ->
+			respond(400, Req2, State#state{handler_state=NewHandlerState, error=bad_request});
+		{{error, Reason}, Req2, NewHandlerState} ->
+			respond(400, Req2, State#state{handler_state=NewHandlerState, error=Reason});
+		{unsupported, Req2, NewHandlerState} ->
+			respond(415, Req2, State#state{handler_state=NewHandlerState, error=unsupported});
+		{halt, Req2, NewHandlerState} ->
+			terminate(Req2, State#state{handler_state=NewHandlerState})
+	catch
+		Class:Error ->
+			error_terminate({Class, Error}, parse_body, Req, State)
+	end.
+
+accept_resource(Method, Req, #state{handler=Handler, handler_state=HandlerState} = State) ->
+	try Handler:Method(Req, HandlerState) of
 		{ok, Req2, NewHandlerState} ->
 			render(render_result, Req2, State#state{handler_state=NewHandlerState});
 		{{see, Url}, Req2, NewHandlerState} when Method =:= post ->
@@ -252,8 +272,8 @@ accept_resource(ContentType, Method, Req, #state{handler=Handler, handler_state=
 			respond(400, Req2, State#state{handler_state=NewHandlerState, error=bad_request});
 		{{error, Reason}, Req2, NewHandlerState} ->
 			respond(400, Req2, State#state{handler_state=NewHandlerState, error=Reason});
-		{halt, Req2} ->
-			stop(Req2, State)
+		{halt, Req2, NewHandlerState} ->
+			terminate(Req2, State#state{handler_state=NewHandlerState})
 	catch
 		Class:Error ->
 			error_terminate({Class, Error}, Method, Req, State)
@@ -280,12 +300,6 @@ method_alias(<<"PATCH">>)  -> {ok, patch};
 method_alias(<<"DELETE">>) -> {ok, delete};
 method_alias(_)            -> error.
 
-method_arity(Method) ->
-	case (Method =:= put) or (Method =:= post) or (Method =:= patch) of
-		true -> 3;
-		false -> 2
-	end.
-
 call(M, F, A, Default) ->
 	case erlang:function_exported(M, F, length(A)) of
 		true -> apply(M, F, A);
@@ -311,23 +325,36 @@ respond(Status, Req, #state{env=Env,
 			error_terminate({Class, Error}, render_error, Req, State)
 	end;
 
-respond(Status, Req, #state{env = Env}) ->
+respond(Status, Req, State) ->
 	{ok, Req2} = cowboy_req:reply(Status, Req),
-	{ok, Req2, Env}.
+	terminate(Req2, State).
 
-build_content_type({Type, Subtype, Params}) ->
-	[Type, $/, Subtype | [[<<"; ">>, Attr, $=, Value] || {Attr, Value} <- Params]].
-
-stop(Req, #state{env=Env}) -> {ok, Req, Env}.
-
-error_terminate(Reason, Method, Req, #state{handler=Module, handler_state=State, env=Env}) ->
+error_terminate(Reason, Method, Req, #state{handler=Module, handler_state=HandlerState} = State) ->
 	_ = error_logger:error_report([
 		moo_rest_crashed,
 		{reason, Reason},
 		{module, Module},
 		{method, Method},
-		{state, State},
+		{state, HandlerState},
 		{stacktrace, erlang:get_stacktrace()}
 	]),
 	{ok, Req2} = cowboy_req:reply(500, Req),
-	{ok, Req2, Env}.
+	terminate(Req2, State).
+
+terminate(Req, #state{env=Env, handler_state='$pre_init'}) ->
+	{ok, Req, Env};
+terminate(Req, #state{env=Env, handler=Handler, handler_state=HandlerState}) ->
+	try call(Handler, rest_terminate, [cowboy_req:lock(Req), HandlerState], ok) of
+		_ -> ok
+	catch
+		Class:Error ->
+			_ = error_logger:error_report([
+				moo_rest_crashed,
+				{reason, {Class, Error}},
+				{module, Handler},
+				{method, rest_terminate},
+				{state, HandlerState},
+				{stacktrace, erlang:get_stacktrace()}
+			])
+	end,
+	{ok, Req, Env}.
